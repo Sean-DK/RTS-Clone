587c315504bdeb013763efe6443e363655d08a2a
Sean Gaffney <smgaffney@neo.rr.com>
2017-08-28 23:11:04 -0400

    Added class declaration

---
+++ Structure.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "StructureCommand.h"
+
+class Structure {
+
+};
---
+++ StructureCommand.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+class StructureCommand {
+
+};
