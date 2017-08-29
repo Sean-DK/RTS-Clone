b072770a0b9bd0dc20ea5205eb2c1bfa7089b670
Sean Gaffney <smgaffney@neo.rr.com>
2017-08-29 17:05:35 -0400

    Working on adding tests

--- RTS_Clone.cpp
+++ RTS_Clone.cpp
@@ -2,40 +2,58 @@
 #include "Unit.h"
 #include "Resource.h"
 
+#include <iostream>
 
 int main()
 {
-	//main window
-	sf::RenderWindow window(sf::VideoMode(768, 640), "Window");
 
-	//TEST AREA
-	std::vector<Unit> unitList;
-	Unit testUnit("circle", UnitType::Worker, 150, 32);
-	unitList.push_back(testUnit);
-	Unit unitTwo("second", UnitType::Army, 300, 48);
-	unitList.push_back(unitTwo);
-	//
-
-
-	while (window.isOpen())
+ 	//Test block
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed) {
-				window.close();
-			}
-		}
+		sf::RenderWindow window(sf::VideoMode(768, 640), "Window");
 
-		window.clear();
-		//draw
-		for (int i = 0; i < unitList.size(); i++) {
-			unitList[i].setPosition(i * 150, i * 150);
-			window.draw(unitList[i].shape);
-		}
+
 		//
-		window.display();
+
+		while (window.isOpen())
+		{
+			sf::Event event;
+			while (window.pollEvent(event))
+			{
+				if (event.type == sf::Event::Closed) {
+					window.close();
+				}
+			}
+
+			window.clear();
+			//draw
+			window.display();
+		}
 	}
+	//End test block
+
+	//Test block
+	{
+		sf::RenderWindow window(sf::VideoMode(768, 640), "Window");
+
+
+		//
+
+		while (window.isOpen())
+		{
+			sf::Event event;
+			while (window.pollEvent(event))
+			{
+				if (event.type == sf::Event::Closed) {
+					window.close();
+				}
+			}
+
+			window.clear();
+			//draw
+			window.display();
+		}
+	}
+	//End test block
 
 	return 0;
 }
