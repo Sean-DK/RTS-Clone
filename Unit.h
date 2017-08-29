15188177ca4ff8806f2bcbc5258ec67ba4eaea75
Sean Gaffney <smgaffney@neo.rr.com>
2017-08-28 23:00:14 -0400

    Added some basic elements

--- Unit.h
+++ Unit.h
@@ -1,4 +1,26 @@
 #pragma once
 
+#include <string>
+#include <vector>
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
+#include "Command.h"
+#include "UnitCommand.h"
+
+enum UnitType {
+	Worker,
+	Army
+};
+
+class Unit {
+	sf::CircleShape shape;
+	sf::FloatRect rect;
+
+	std::string name;
+	UnitType type;
+	int maxHealth;
+	int currentHealth;
+
+	bool selected = false;
+	std::vector<Command> commandQueue;
+};
