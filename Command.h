25c1335a8b7df363f9e71a0b300b443f7c697963
Sean Gaffney <smgaffney@neo.rr.com>
2017-08-30 00:21:33 -0400

    Added member variables and constructor

--- Command.h
+++ Command.h
@@ -1,5 +1,34 @@
 #pragma once
 
+#include "Unit.h"
+
+enum CommandType {
+	Move,
+	Attack
+};
+
+class Point {
+public:
+	int x;
+	int y;
+
+	Point() {}
+	Point(int x, int y)
+		: x(x), y(y) {}
+
+};
+
 class Command {
+public:
+	CommandType type;
+	Point endPoint;
+	//Unit target;
+	bool completed = false;
+
+	Command() {}
+	Command(CommandType type, Point endPoint)
+		: type(type), endPoint(endPoint) {}
+	//Command(CommandType type, Unit target)
+		//: type(type), target(target) {}
 
 };
