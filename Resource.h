4b485a138904209e22ae0fddc4408563cd3d1557
Sean Gaffney <smgaffney@neo.rr.com>
2017-09-01 12:45:56 -0400

    Added member variables and constructors

--- Resource.h
+++ Resource.h
@@ -3,7 +3,18 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 
+enum ResourceType {
+	Blue;
+};
+
 class Resource {
 public:
+	int maxAmount;
+	int currentAmount;
+	ResourceType type;
 
+	Resource()
+		: maxAmount(5000), currentAmount(5000), type(ResourceType::Blue) {}
+	Resource(int maxAmount, int currentAmount)
+		: maxAmount(maxAmount), currentAmount(currentAmount), type(ResourceType::Blue) {}
 };
