15af9f5ec4ede637f405adea832525e330e41fc9
Sean Gaffney <smgaffney@neo.rr.com>
2017-09-20 20:50:09 -0400

    Added private section and accessors

--- Resource.h
+++ Resource.h
@@ -8,21 +8,28 @@ enum ResourceType {
 };
 
 class Resource {
-public:
+private:
 	int maxAmount;
 	int currentAmount;
 	ResourceType type;
 	sf::CircleShape shape;
 
-	Resource() 
-		: maxAmount(500), currentAmount(500), type(ResourceType::Blue) {
-		shape.setRadius(24);
-		shape.setFillColor(sf::Color::Blue);
-	}
+public:
 
+	//constructor
 	Resource(int maxAmount)
 		: maxAmount(maxAmount), currentAmount(maxAmount), type(ResourceType::Blue) {
 		shape.setRadius(24);
 		shape.setFillColor(sf::Color::Blue);
 	}
+
+	//Getters
+	const int getMaxAmount() { return maxAmount; }
+	const int getCurrentAmount() { return currentAmount; }
+	const sf::CircleShape* getShape() { return &shape; }
+	const ResourceType getType() { return type; }
+
+	//Setters
+	void setPosition(int x, int y) { shape.setPosition(x, y); }
+	void setAmount(int x) { currentAmount = x; }
 };
